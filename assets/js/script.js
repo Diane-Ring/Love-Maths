23cbfdd54a75951ca4c7fb430db9fadae25364f7
Diane-Ring <diane-hawkins@hotmail.co.uk>
2025-11-11 19:33:49 +0000

    removed spaces in division code

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,8 +82,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else if (operator === "/") {
-        return [operand1 / operand2, "division"];
+     } else if (operator ==="/") {
+		return [operand1 / operand2, "division"];
     
     } else {
         alert(`Unimplemented operator ${operator}`);
